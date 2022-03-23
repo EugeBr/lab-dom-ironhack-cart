15a220cff87f1287d88034ff5105a570d452b72c
Maria Eugenia Bordenave <eugebordenave@gmail.com>
2022-03-23 15:34:10 +0100

    +Bonus

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-  const price = product.querySelector('.price span').innerHTML;
-  const quantity = product.querySelector('.quantity input').value;//!for inputs access 'value'.
+ // console.log('Calculating subtotal, yey!');
+  const price = +product.querySelector('.price span').textContent;
+  const quantity = +product.querySelector('.quantity input').value;//!for inputs access 'value'.
   const subtotal = price * quantity;
-  product.querySelector('.subtotal span').innerHTML = subtotal;
+  product.querySelector('.subtotal span').textContent = subtotal.toFixed(2);
 
   return subtotal;
 }
@@ -26,14 +26,15 @@ function calculateAll() {
   }
   
   // ITERATION 3
-  document.querySelector('#total-value span').innerHTML = totalPrice;
+  document.querySelector('#total-value span').textContent = totalPrice;
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
+  const target = event.target;
+  //console.log('The target in remove is:', target);
+  target.closest('tr').remove();
   //... your code goes here
 }
 
@@ -41,11 +42,28 @@ function removeProduct(event) {
 
 function createProduct() {
   //... your code goes here
+  const tbody = document.querySelector('tbody');
+  const template = document.getElementById('template');
+  const clone = document.importNode(template.content, true);
+  const name = document.querySelector('.create-product > td input');
+  const price = document.querySelector('.create-product td:nth-of-type(2) input');
+  clone.querySelector('.name span').textContent = name.value;
+  clone.querySelector('.price span').textContent = price.value;
+  clone.querySelector('.btn-remove').addEventListener('click', removeProduct);
+  name.value = '';
+  price.value = '';
+  tbody.append(clone);
 }
 
-window.addEventListener('load', () => {
+//window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
+  const removeButtons = document.querySelectorAll('.btn-remove');
 
+  document.getElementById('create').addEventListener('click', createProduct);
+
+  removeButtons.forEach((button) => {
+    button.addEventListener('click', removeProduct);
+  });
   //... your code goes here
-});
+//});
